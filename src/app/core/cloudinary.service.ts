82853f5ac2854fdd14a44deb08722b73168d4fa9
Evert Smit <admin@sidhe.net>
2020-09-19 17:15:16 +0200

    Update cloudinary.service.ts
    removed dependency on angular/http module

--- src/app/core/cloudinary.service.ts
+++ src/app/core/cloudinary.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Response} from '@angular/http';
+//import {Response} from '@angular/http';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
